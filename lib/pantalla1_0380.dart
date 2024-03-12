fd4c431a7d3ebf9be7daf397c69f0a0124d85cd1
VANYA_ANETTE_MATUS_GAMBOA <a.21308051280380@cbtis128.edu.mx>
2024-03-12 18:43:09 +0000

    actualizado

--- lib/pantalla1_0380.dart
+++ lib/pantalla1_0380.dart
@@ -1,4 +1,6 @@
 //pantalla1_0380
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
 class pantalla1_0380 extends StatelessWidget {
@@ -8,17 +10,24 @@ class pantalla1_0380 extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Pantalla 1 Matus 0380"),
+        title: const Text("Card Pantalla 1 Matus 0380"),
         backgroundColor: Color(0xffbe89a8),
       ),
       body: Center(
-        child: Column(
-          children: [
-            Container(
-              child: const Text("Matus Ejemplo"),
-            )
-          ],
-        ),
+        child: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+            color: Color(0xffe0a4d1),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Text(
+                "Card Matus",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: Color(0xfff4cdec),
+                ),
+              ),
+            )),
       ),
     );
   } //fin widget
--- lib/pantalla2_0380.dart
+++ lib/pantalla2_0380.dart
@@ -12,14 +12,25 @@ class pantalla2_0380 extends StatelessWidget {
         backgroundColor: Color(0xffbe89a8),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {}, //si preciona boton
-              child: const Text("pantalla"),
-            )
-          ],
+        child: Container(
+          width: double.infinity,
+          height: 300,
+          color: Color(0xffb46fae),
+          child: Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              color: Color(0xffc692b7),
+              margin: EdgeInsets.all(32),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  "Card cotainer Matus 0380",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Color(0xffd5d3d5),
+                  ),
+                ),
+              )),
         ),
       ),
     );
